03721b9f6ff07254ae645530404d56f2f1180e0f
Amira Bahou <amira2608@hotmail.fr>
2024-10-10 18:46:58 +0200

    mise en forme fichier

--- app copy.py
+++ app copy.py
@@ -2,12 +2,9 @@
 from fastapi import FastAPI
 import joblib
 import pandas as pd
-import numpy as np
 
-# Charger l'imputer, le scaler et le modèle
-imputer = joblib.load("imputer.joblib")
-scaler = joblib.load("scaler.joblib")
-model = joblib.load("logistic_regression_model.joblib")
+# Charger le modèle
+pipeline = joblib.load("pipeline.joblib")
 
 # Initialiser l'API
 app = FastAPI()
@@ -24,20 +21,15 @@ def get_prediction_label(probability, threshold=0.53):
 # Route API pour la prédiction
 @app.post("/predict")
 async def predict(features: dict):
-    # Convertir les données en DataFrame
+    # Convertir les données en DataFrame pour les passer au modèle
     X = pd.DataFrame([features])
     
-    # Imputation
-    quantitative_columns = X.select_dtypes(include=[np.number]).columns
-    X[quantitative_columns] = imputer.transform(X[quantitative_columns])
-
-    # Normalisation
-    X = scaler.transform(X)
-
     # Prédiction
-    prob = model.predict_proba(X)[:, 1]  # Probabilité de défaut (classe 1)
+    prob = pipeline.predict_proba(X)[:, 1]  # Probabilité de défaut (classe 1)
     
     # Calculer la classe
     label = get_prediction_label(prob[0], threshold=0.53)
     
     return {"probability": prob[0], "class": label}
+
+
